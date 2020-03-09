2bc5bb36bbb09c47b44757565166687a7bd907a4
TFX255 <TFX255@DESKTOP-B9SEJ1R>
2020-03-09 12:03:45 +0900

    0309 12:03 권우성 캘린더 부분수정

--- admin/src/admin/shoes/app/controller/FrontController.java
+++ admin/src/admin/shoes/app/controller/FrontController.java
@@ -13,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import admin.shoes.app.command.DelHoliday;
 import admin.shoes.app.command.DeleteProductCommand;
 import admin.shoes.app.command.DeleteSmemCommand;
+import admin.shoes.app.command.GetHoliday;
 import admin.shoes.app.command.InsertEventCommand;
 import admin.shoes.app.command.InsertProductCommand;
 import admin.shoes.app.command.InsertSmemCommand;
@@ -92,7 +93,7 @@ public class FrontController extends HttpServlet {
 
 		// 일정 등록
 		cont.put("/ajax/SetHoliday.do", new SetHoliday()); //일정 업로드
-		//		cont.put("/ajax/GetHoliday.do", new GetHoliday()); //휴일 가져오기
+		cont.put("/ajax/GetHoliday.do", new GetHoliday()); //휴일 가져오기
 		cont.put("/ajax/DelHoliday.do", new DelHoliday()); //휴일 삭제
 		cont.put("/ajax/getDeptCnt.do", new getDeptCnt()); //차트데이터
 
